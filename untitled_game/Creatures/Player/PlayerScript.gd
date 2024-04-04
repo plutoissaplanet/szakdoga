9479ed6029665f0628e56ce8a403a18b76c604f1
plutoissaplanet <epolanek@gmail.com>
2024-04-04 15:13:36 +0200

    Nanogram játék befejezve

--- untitled_game/Creatures/Player/PlayerScript.gd
+++ untitled_game/Creatures/Player/PlayerScript.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 
-const SPEED = 200.0
+const SPEED = 180.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
--- untitled_game/MiniGames/Nanogram/nanogram_general.gd
+++ untitled_game/MiniGames/Nanogram/nanogram_general.gd
@@ -5,52 +5,69 @@ extends Node2D
 @onready var problem = []
 @onready var container1 = get_node("Container")
 @onready var container2 = get_node("Container/Container")
-	
+@onready var hbox= get_node("Container/HBoxContainer")
+@onready var vbox= get_node("Container/VBoxContainer")
+
+
 @onready var riddle1 = [
-		[0, 0, 1, 1, 0, 0, 1, 1, 1, 0],
-		[0, 1, 0, 1, 0, 1, 0, 0, 0, 1],
-		[1, 0, 0, 1, 0, 0, 0, 0, 1, 0],
-		[0, 0, 0, 1, 0, 0, 0, 1, 0, 0],
-		[0, 0, 0, 1, 0, 0, 1, 0, 0, 0],
-		[0, 0, 0, 1, 0, 0, 1, 0, 0, 0],
-		[0, 0, 0, 1, 0, 0, 0, 1, 0, 0],
-		[0, 0, 0, 1, 0, 0, 0, 0, 1, 0],
-		[0, 0, 0, 1, 0, 1, 0, 0, 0, 1],
-		[0, 0, 1, 1, 1, 0, 1, 1, 1, 0]
+		[[1], [1], [1, 1], [10],[1], [1, 1], [1, 2, 1], [1,1,1,1],[1,1,1,1], [1,1], [1, 3],[1,1,1,1], [1,1,1], [1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[3,3]],
+		[ 0, 0, 1, 1, 0, 0, 1, 1, 1, 0],
+		[ 0, 1, 0, 1, 0, 1, 0, 0, 0, 1],
+		[ 1, 0, 0, 1, 0, 0, 0, 0, 1, 0],
+		[ 0, 0, 0, 1, 0, 0, 0, 1, 0, 0],
+		[ 0, 0, 0, 1, 0, 0, 1, 0, 0, 0],
+		[ 0, 0, 0, 1, 0, 0, 1, 0, 0, 0],
+		[ 0, 0, 0, 1, 0, 0, 0, 1, 0, 0],
+		[ 0, 0, 0, 1, 0, 0, 0, 0, 1, 0],
+		[ 0, 0, 0, 1, 0, 1, 0, 0, 0, 1],
+		[ 0, 0, 1, 1, 1, 0, 1, 1, 1, 0]
 	]
 	
 @onready var riddle2 = [
-	[1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+	[[1, 1], [2, 2], [1,1,1,1], [1,1,1], [1,1], [1,1], [1,1], [1,1,1],[1,1,1,1],[2,2], [1], [1,1], [1,1], [3,3], [1,1], [1,1], [1,1,1],[1,1,1,1],[1,1,1,1], [3,2]],
+	[0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+	[0, 0, 0, 0, 1, 0, 1, 0, 0, 0],
+	[0, 0, 0, 1, 0, 0, 0, 1, 0, 0],
+	[1, 1, 1, 0, 0, 0, 0, 0, 1, 1],
+	[0, 1, 0, 0, 0, 0, 0, 0, 0, 1],
+	[0, 0, 1, 0, 0, 0, 0, 0, 1, 0],
+	[0, 0, 0, 1, 0, 1, 0, 1, 0, 0],
+	[0, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+	[0, 1, 0, 1, 0, 0, 0, 1, 0, 1],
+	[1, 1, 1, 0, 0, 0, 0, 0, 1, 1]
+	
 ]
 @onready var riddle3 = [
-	[1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+	[[0], [0], [1], [3], [2,1], [2,4], [2,1], [3], [1], [0], [0], [1], [3], [2,2], [2,2],[2,2], [1], [1], [1], [1]],
 	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+	[0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+	[0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+	[0, 0, 0, 1, 1, 0, 1, 1, 0, 0],
+	[0, 0, 1, 1, 0, 0, 0, 1, 1, 0],
+	[0, 0, 0, 1, 1, 0, 1, 1, 0, 0],
+	[0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 1, 0, 0, 0, 0]
 ]
 
 @onready var riddle4 = [
-	[1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[[5], [1], [1], [5], [0], [4,1], [1,1,1], [1,1,1], [1,1,1], [1,2], [1,5], [1,1], [1,1], [1,1], [4,2], [1,1], [1,1], [1,1],[1,4], [0]],
+	[1, 0, 0, 0, 0, 1, 1, 1, 1, 1],
+	[1, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+	[1, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+	[1, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+	[1, 1, 1, 1, 0, 0, 1, 1, 0, 0],
+	[0, 0, 0, 1, 0, 0, 0, 0, 1, 0],
+	[0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
+	[0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
+	[0, 0, 0, 1, 0, 1, 1, 1, 1, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+	
 ]
 	
-@onready var solutions = [riddle1, riddle2, riddle3, riddle4]
+@onready var solutions = [riddle1.slice(1,riddle1.size()+1), riddle2.slice(1, riddle2.size()+1), riddle3.slice(1, riddle3.size()+1), riddle4.slice(1, riddle4.size()+1)]
+@onready var solutions_unsliced = [riddle1, riddle2, riddle3, riddle4]
 @onready var riddle_index = randi_range(0, solutions.size()-1)
 
 @onready var riddle_rows = solutions[riddle_index].size()
@@ -59,14 +76,18 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var spacing = 2
+	text()
 	initialize_problem()
-	print(solutions[riddle_index])
 
+	print("selected solutions: ", solutions[riddle_index])
+	print("riddle number: ", riddle_index)
+	print("riddle_rows: ", riddle_rows, "riddle_cols ", riddle_cols)
+	
 	for i in range(riddle_rows):
 		var row = []
 		for j in range(riddle_cols):
 			rect =  ColorRect.new()
-			rect.size=Vector2(20,20)
+			rect.size=Vector2(21,21)
 			rect.position=Vector2(j*(rect.size.x+spacing), i*(rect.size.y+spacing))
 			container2.add_child(rect)
 			row.append(rect)
@@ -77,6 +98,7 @@ func _ready():
 	
 func _process(_delta):
 	gameplay()
+
 	
 func connect_cells():
 	for i in range(riddle_rows):
@@ -90,7 +112,7 @@ func initialize_problem():
 			row.append(0)
 		problem.append(row)
 
-func onClick(event,i,j):
+func onClick(_event,i,j):
 	if Input.is_action_pressed("room_changer_click"):
 		cells[i][j].color=Color(0,0,0)
 		problem[i][j]=1
@@ -115,3 +137,49 @@ func size_and_position():
 	container1.position=cont1_pos
 	container2.position=cont2_pos
 	
+	vbox.position=Vector2(cont2_pos.x-5, cont2_pos.y-5)
+	hbox.position=Vector2(cont2_pos.x, cont2_pos.y-100)
+	
+	print("cont1 pos: ", container1.position)
+	print("cont2 pos: ", container2.position)
+	
+	
+func text():
+	var temp = solutions_unsliced[riddle_index]
+	
+	var temp_pop
+	var max_arr_size = 0
+	#print("temp", temp)
+	
+	for i in range(temp[0].size()/2):
+		var arr_size = temp[0][i].size()
+		if arr_size > max_arr_size:
+			max_arr_size = arr_size
+	
+	print(temp[0].size())
+	#print(" ")
+	
+	for i in range(temp[0].size()):
+		
+		if i > riddle_cols-1:
+			temp_pop= temp[0][i]
+			var row_text=""
+			for j in range(temp_pop.size()):
+				row_text += str(temp_pop[j])+" "
+			#print(row_text)
+			var txt = Label.new()
+			txt.text=row_text.left(row_text.length()-1)
+			vbox.add_child(txt)
+		else:
+			temp_pop=temp[0][i]
+			var horiz_text=""
+			var temppop_size = temp_pop.size()
+			
+			for j in range(max_arr_size-temppop_size):
+				temp_pop.push_front(" ")
+			for j in range(temp_pop.size()):
+				horiz_text += str(temp_pop[j])+"\n"
+			#print(row_text)
+			var txt = Label.new()
+			txt.text=horiz_text.left(horiz_text.length()-1)
+			hbox.add_child(txt)
